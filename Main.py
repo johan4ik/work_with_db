6d354f4a0d418c0f23b42e287c5bd125ed98c884
Алексей Павлов <alex.pawlof@yandex.ru>
2021-12-08 01:19:38 +0300

    refactor

--- Main.py
+++ Main.py
@@ -2,94 +2,109 @@ import psycopg2
 from config import host, user, password, db_name
 import csv
 
-try:
-    # connect to exist database
-    connection = psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=db_name
-    )
-    connection.autocommit = True
-    cur = connection.cursor()
 
-    # Автоматическое создание и заполнение таблицы gender_train
-
-    with open("цсвшки/gender_train_cut.csv", encoding="UTF-8") as data:
+def read_csv(path):
+    with open(path, encoding="UTF-8") as data:
         lst = [i for i in data.read().split('\n')]
         columns = lst[0].split(",")
-        cur.execute(f"CREATE TABLE gender_train ({columns[0]} INTEGER ," +
-                    f"{columns[1]} INTEGER)")
         del lst[0]
-        for i in lst:
-            columns = i.split(",")
-            cur.execute(f"INSERT INTO " +
-                        f"gender_train (customer_id,gender) "
-                        f"VALUES ({int(columns[0])},{int(columns[1])})")
+        del lst[-1]
+    return lst, columns
 
-    # Автоматическое создание и заполнение таблицы tr_mcc_codes
 
-    with open("цсвшки/tr_mcc_codes.csv", encoding="UTF-8") as data:
-        lst = [i for i in data.read().split('\n')]
-        columns = lst[0].split(";")
-        cur.execute(f"CREATE TABLE tr_mcc_codes ({columns[0]} INTEGER ," +
-                    f"{columns[1]} VARCHAR(300))")
-        del lst[0]
-        for i in lst:
-            columns = i.split(";")
-            cur.execute(f"INSERT INTO " +
-                        f"tr_mcc_codes (mcc_code,mcc_description) "
-                        f"VALUES ({int(columns[0])},'{columns[1]}')")
+def create_gender_train_table(cur):
+    lst, columns = read_csv("csv_files/gender_train_cut.csv")
+    cur.execute(f"CREATE TABLE gender_train ({columns[0]} INTEGER ," +
+                f"{columns[1]} INTEGER)")
+    for i in lst:
+        column = i.split(",")
+        cur.execute(f"INSERT INTO " +
+                    f"gender_train (customer_id,gender) "
+                    f"VALUES ({int(column[0])},{int(column[1])})")
 
-    # Автоматическое создание и заполнение таблицы tr_types
 
-    with open("цсвшки/tr_types.csv", encoding="UTF-8") as data:
-        lst = [i for i in data.read().split('\n')]
-        columns = lst[0].split(";")
-        cur.execute(f"CREATE TABLE tr_types ({columns[0]} INTEGER ," +
-                    f"{columns[1]} VARCHAR(300))")
-        del lst[0]
-        for i in lst:
-            columns = i.split(";")
-            cur.execute(f"INSERT INTO " +
-                        f"tr_types (tr_type,tr_description) "
-                        f"VALUES ({int(columns[0])},'{columns[1]}')")
+def create_tr_mcc_codes_table(cur):
+    lst, columns = read_csv("csv_files/tr_mcc_codes.csv")
+    cur.execute(f"CREATE TABLE tr_mcc_codes ({columns[0]} INTEGER ," +
+                f"{columns[1]} VARCHAR(300))")
+    for i in lst:
+        column = i.split(";")
+        cur.execute(f"INSERT INTO " +
+                    f"tr_mcc_codes (mcc_code,mcc_description) "
+                    f"VALUES ({int(column[0])},'{column[1]}')")
 
-    # Автоматическое создание и заполнение таблицы transactions
 
-    with open("цсвшки/transactions_cut.csv", encoding="UTF-8") as data:
-        lst = [i for i in data.read().split('\n')]
-    columns = lst[0].split(",")
+def create_transactions_table(cur):
+    lst, columns = read_csv("csv_files/transactions_cut.csv")
     cur.execute(f"create table transactions " +
                 f"({columns[0]} integer, {columns[1]} varchar(20),"
                 f"{columns[2]}_{columns[3]} varchar(20) , {columns[4]} real)")
-
-    del lst[0]
     for i in lst:
-        col = i.split(",")
+        column = i.split(",")
         cur.execute(f"INSERT into " +
                     f"transactions (customer_id,tr_datetime,mcc_code_tr_type,amount)"
-                    f"values ({int(col[0])},'{col[1]}','{col[2]} ; {col[3]}',{float(col[4])})")
+                    f"values ({int(column[0])},'{column[1]}','{column[2]} ; {column[3]}',{float(column[4])})")
 
+
+def create_tr_types_table(cur):
+    lst, columns = read_csv("csv_files/tr_types.csv")
+    cur.execute(f"CREATE TABLE tr_types ({columns[0]} INTEGER ," +
+                f"{columns[1]} VARCHAR(300))")
+    for i in lst:
+        column = i.split(";")
+        cur.execute(f"INSERT INTO " +
+                    f"tr_types (tr_type,tr_description) "
+                    f"VALUES ({int(column[0])},'{column[1]}')")
+
+
+def calculate_variance(cur):
     cur.execute("select variance(a.amount) " +
                 "from(select customer_id,count(mcc_code_tr_type),amount "
                 "from transactions "
                 "group by customer_id,mcc_code_tr_type,amount "
                 "having amount<0 and count(mcc_code_tr_type)>=10) as a")
 
-    print("Дисперсия по столбцу amount:")
     variance = cur.fetchone()
-    with open("цсвшки/variance.csv", "w", newline="") as file:
-        writer = csv.writer(file)
+    with open("csv_files/variance.csv", "w", newline="") as csv_writer:
+        writer = csv.writer(csv_writer)
         writer.writerow(["variance"])
         writer.writerow(variance)
 
-    print(*variance)
+    return variance[0]
 
-except Exception as _ex:
-    print("[INFO] Error while working with PostgreSQL", _ex)
-finally:
-    if connection:
-        connection.close()
-        cur.close()
-        print("[INFO] PostgreSQL connection closed")
+
+def main():
+    try:
+        # connect to exist database
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
+        )
+        connection.autocommit = True
+        cur = connection.cursor()
+
+        # create_tr_types_table(cur)
+        #
+        # create_gender_train_table(cur)
+        #
+        # create_transactions_table(cur)
+        #
+        # create_tr_mcc_codes_table(cur)
+
+        variance = calculate_variance(cur)
+
+        print(f"Дисперсия : {variance}")
+
+    except Exception as _ex:
+        print("[INFO] Error while working with PostgreSQL", _ex)
+    finally:
+        if connection:
+            connection.close()
+            cur.close()
+            print("[INFO] PostgreSQL connection closed")
+
+
+if __name__ == "__main__":
+    main()
